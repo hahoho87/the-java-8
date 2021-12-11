15fadacd06e07ce4a0f3759f00f7b1c14f62e442
hahoho87 <bbaing4564@gmail.com>
2021-12-11 22:19:28 +0900

    add java functional interface tests

--- src/test/java/com/hahoho87/thejava8/FooTest.java
+++ src/test/java/com/hahoho87/thejava8/FooTest.java
@@ -1,7 +1,55 @@
 package com.hahoho87.thejava8;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class FooTest {
+
+    @Test
+    void functionTest() {
+        Function<Integer, Integer> add2Function = (i) -> i + 2;
+        assertThat(add2Function.apply(5)).isEqualTo(7);
+
+        UnaryOperator<Integer> multiply3Function = i -> i * 3;
+        BinaryOperator<Integer> add3Function = (x, y) -> x + y + 3;
+
+        assertThat(multiply3Function.apply(5)).isEqualTo(15);
+        assertThat(add3Function.apply(3, 5)).isEqualTo(11);
+    }
+
+    @Test
+    void composeFunctionTest() {
+        Function<Integer, Integer> add2Function = (i) -> i + 2;
+        Function<Integer, Integer> multiply2Function = (i) -> i * 2;
+
+        Function<Integer, Integer> add2AndThenMultiply2 = add2Function.andThen(multiply2Function);
+        assertThat(add2AndThenMultiply2.apply(5)).isEqualTo(14);
+
+        Function<Integer, Integer> add2ComposeMultiply2 = add2Function.compose(multiply2Function);
+        assertThat(add2ComposeMultiply2.apply(5)).isEqualTo(12);
+    }
+
+    @Test
+    void consumerTest() {
+        Consumer<String> printString = (s -> System.out.println("s = " + s));
+        printString.accept("hello");
+    }
+
+    @Test
+    void supplierTest() {
+        Supplier<String> getHelloString = () -> "hello";
+        assertThat(getHelloString.get()).isEqualTo("hello");
+    }
+
+    @Test
+    void PredicateTest() {
+        Predicate<Integer> isOddNumber = (i) -> i % 2 != 0;
+        assertThat(isOddNumber.test(5)).isTrue();
+        assertThat(isOddNumber.test(6)).isFalse();
+    }
+
 
 }
